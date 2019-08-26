bf2189315b4f12f4f5f9765487a48c09d88e5b56
Elxctronick <nickpp1111@yahoo.com>
2019-08-25 22:48:20 -0400

    Update Main.php

--- src/CustomCraftingUI/Main.php
+++ src/CustomCraftingUI/Main.php
@@ -38,7 +38,7 @@ class Main extends PluginBase implements Listener {
 		$name = $sender->getName();
 		$level->addSound(new EndermanTeleportSound($pos1));
 		$level->addParticle(new LavaParticle($pos1));
-		$inv->removeItem(Item::get(131,1,1));
+		$inv->removeItem(Item::get(41,1,256));
 		$this->getServer()->broadcastMessage("§6$name §8Crafted a §6Royle Sword!");
 		$inv = addItem(Item::get(283,1,1));
 	}
@@ -46,3 +46,36 @@ class Main extends PluginBase implements Listener {
 		$sender->sendMessage("§6 You don't have the items required to craft the Royale Sword");
 	}
 break;
+	case 2:
+				
+		$inv = $sender->getInventory();
+		       if($inv->contains(Item::get(41,1,256))){
+		$level = $sender->getLevel();
+		$x = $sender->getX();
+		$y = $sender->getY();
+		$z = $sender->getZ();
+		$pos = new Vector3($x, $y + 2, $z);
+		$pos1 = new Vector3($x, $y, $z);
+		$name = $sender->getName();
+		$level->addSound(new EndermanTeleportSound($pos1));
+		$level->addParticle(new LavaParticle($pos1));
+		$inv->removeItem(Item::get(41,1,256));
+		$this->getServer()->broadcastMessage("§6$name §8Crafted a §6Royle Helmet!");
+		$inv = addItem(Item::get(283,1,1));
+	}
+	}else{
+		$sender->sendMessage("§6 You don't have the items required to craft the Royale helmet");
+	}
+break;
+				$form->setTitle("§6Craft");
+				$form->setContent("§ecraft some sepcial items!");
+				$form->addButton("§8Royale Sword", 1);
+				$form->addButton("§8Prince Helmet", 2);
+				$form->sendToPlayer($sender);
+			 }else{
+				 $sender->sendMessage("§cYou are not In-Game.");
+			 }
+			 return true;
+		}
+	}
+}
